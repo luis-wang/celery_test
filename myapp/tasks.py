b151b6d8beebe9bcb3a79778ee46a697ab73eaa8
luis wang <wud1234@gmail.com>
2014-10-29 09:21:41 +0800

    update

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -4,8 +4,8 @@ from celery import Celery
 
 #first args is the name of the current module
 #app = Celery('tasks', broker='amqp://guest@localhost//')
-#app = Celery('tasks', backend='redis://localhost', broker='amqp://')
-app = Celery('tasks', backend='amqp', broker='amqp://guest@localhost//')
+#app = Celery('tasks', backend='redis://121.40.208.251', broker='amqp://')
+app = Celery('tasks', backend='amqp', broker='amqp://guest@121.40.208.251//')
 
 app.conf.CELERY_TASK_SERIALIZER = 'json'
 app.conf.update(
@@ -16,6 +16,13 @@ app.conf.update(
     CELERY_ENABLE_UTC=True,
 )
 
+'''
+@app.task(bind=True)
+def upload_files(self, filenames):
+    for i, file in enumerate(filenames):
+        print '*(*(*', self.state
+        self.update_state(state='PROGRESS',  meta={'current': i, 'total': len(filenames)})
+'''
 
 
 @app.task
@@ -25,12 +32,23 @@ def add(x, y):
 
 #celery -A tasks worker --loglevel=info
 
+def run_task1():
+    result = add.delay(10, 99)
+    print result.state
+    print 'result = ', result
+    print result.ready()
+    print result.state
+    print result.backend
+    #print result.get(timeout=1)
+
+
 if __name__ == '__main__':
     print 'start..'
-    result = add.delay(100, 99)
-    print 'result = ', result
-    #print result.get(timeout=1)
+    run_task1()
+
+    '''
     result.get(propagate=False)
     print 'track: ',result.traceback
     print result.ready()
+    '''
     print 'end...'
